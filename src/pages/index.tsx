9e4c827056d7636164c29892ca6c1c6847529408
Drayerr <gabrielgrv2015@outlook.com>
2021-09-11 19:27:40 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import avatar from '../images/avatar.svg'
-// import { stripe } from '../services/stripe'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
@@ -27,42 +27,38 @@ export default function Home({ product }: HomeProps) {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
-            {/* <span>for {product.amount} month</span> */}
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={"9.90"}/>
-          {/* <SubscribeButton priceId={product.priceId}/> */}
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
-        {/* <img src="/images/avatar.svg" alt="Girl coding" /> */}
-
         <div>
-        <Image
-          alt={'text'}
-          src={avatar}
-        />
+          <Image
+            alt={'text'}
+            src={avatar}
+          />
         </div>
       </main>
     </>
   )
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const price = await stripe.prices.retrieve('price_1JYDp8DDdwlroJwF4vX72tiO', {
-//     expand: ['product']
-//   })
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JYDp8DDdwlroJwF4vX72tiO', {
+    expand: ['product']
+  })
 
-//   const product = {
-//     priceId: price.id,
-//     amount: new Intl.NumberFormat('en-US', {
-//       style: 'currency',
-//       currency: 'USD'
-//     }).format(price.unit_amount / 100)
-//   }
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
 
-//   return {
-//     props: {
-//       product
-//     }
-//   }
-// }
+  return {
+    props: {
+      product
+    }
+  }
+}
